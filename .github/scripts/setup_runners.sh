0f9a0f9537d484fc2d2b0f58f82407ed7d6a9c79
Peter Zhu <zhujiaxi@amazon.com>
2020-07-20 20:52:25 -0400

    test runners

--- .github/scripts/setup_runners.sh
+++ .github/scripts/setup_runners.sh
@@ -45,15 +45,15 @@ then
 #  sleep 60
 #
 
-  aws configure list
-  aws ssm describe-instance-information --output text
+  #aws configure list
+  #aws ssm describe-instance-information --output text
 
   # Setup VMs to register as runners
   for instance_name2 in $SETUP_INSTANCE
   do
     echo "get runner token and bootstrap on Git"
     instance_runner_token=`curl --silent -H "Authorization: token ${SETUP_TOKEN}" --request POST "${GIT_URL_API}/${GIT_URL_REPO}/actions/runners/registration-token" | jq -r .token`
-    aws ssm send-command --targets Key=Name,Values=$instance_name2 --document-name "AWS-RunShellScript" \
+    aws ssm send-command --targets tag:Name=$instance_name2 --document-name "AWS-RunShellScript" \
                          --parameters '{"commands": ["#!/bin/bash", "sudo su - '${SETUP_AMI_USER}' && cd actions-runner && ./config.sh --unattended --url '${GIT_URL_BASE}/${GIT_URL_REPO}' --labels '${instance_name2}' --token '${instance_runner_token}'", "nohup ./run.sh &"]}' \
                          --output text
     sleep 3
